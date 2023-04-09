e900fdc61a9be1eda29bd4fd2661c2d577b575a1
Bubinoso <luizcamolez@gmail.com>
2023-04-09 10:11:18 -0300

    IMC calculator

---
+++ VSCode/0804 - calcular IMC/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Qual o seu peso em quilos?: ");
+float peso = float.Parse(Console.ReadLine());
+Console.WriteLine("Qual sua altura?: ");
+float altura = float.Parse(Console.ReadLine());
+
+float imc = peso / (altura * altura);
+string estado = "";
+
+if (imc <= 18.5)
+{
+    estado = "magreza";
+}
+else if (imc <= 24.9)
+{
+    estado = "normal";
+}
+else if (imc <= 29.9)
+{
+    estado = "sobrepeso";
+}
+else if (imc <= 39.9)
+{
+    estado = "obesidade";
+}
+else
+{
+    estado = "obesidade grave";
+}
+
+Console.WriteLine($"O seu IMC é {imc.ToString("N2")} kg/m² e o seu estado é: {estado}.");
